d1ba409f6359a67a1639506cd509b6143d7af05d
Fikry Maulana <fikry.maulana@gmail.com>
2025-09-14 12:34:33 +0700

    Fix issue of invalid fileId field when creating product

--- internal/product/usecase/usecase.go
+++ internal/product/usecase/usecase.go
@@ -22,6 +22,16 @@ func NewProductUsecase(productRepository product.IRepositoryProduct, fileReposit
 }
 
 func (u ProductUsecase) CreateProduct(ctx context.Context, req product.CreateProductRequest) (res product.CreateProdutResponse, err error) {
+	// First verify the file exists before creating the product
+	fileData, err := u.fileRepository.GetFileByID(ctx, req.FileID)
+	if err != nil {
+		logrus.WithFields(logrus.Fields{
+			"file_id": req.FileID,
+			"auth_id": req.AuthId,
+			"error":   err.Error(),
+		}).Error("Failed to get file data before creating product")
+		return res, fmt.Errorf("invalid file ID: %w", err)
+	}
 
 	createProduct := entity.Product{
 		AuthID:       req.AuthId,
@@ -34,11 +44,6 @@ func (u ProductUsecase) CreateProduct(ctx context.Context, req product.CreatePro
 	}
 
 	savedProduct, err := u.productRepository.CreateProduct(ctx, createProduct)
-	if err != nil {
-		return res, err
-	}
-
-	fileData, err := u.fileRepository.GetFileByID(ctx, req.FileID)
 	if err != nil {
 		return res, err
 	}
